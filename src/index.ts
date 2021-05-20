ab6d3337f9d3757a72060b8d0c40f6b447bb5141
Tyler Long <tyler4long@gmail.com>
2021-05-20 11:00:35 -0700

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world');
+import RingCentral from '@rc-ex/core';
+import DebugExtension from '@rc-ex/debug';
+
+const rc = new RingCentral({
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+  server: process.env.RINGCENTRAL_SERVER_URL,
+});
+
+(async () => {
+  const debugExtension = new DebugExtension();
+  await rc.installExtension(debugExtension);
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+  console.log(JSON.stringify(rc.token, null, 2));
+})();
